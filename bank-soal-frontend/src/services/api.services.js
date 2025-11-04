72dd205e7334bc046e0c29a1212dc1ceabdaf0e1
ClarenceFer <ferdinandclarence5@gmail.com>
2025-11-04 20:00:40 +0700

    Memperbaharui link vite

--- bank-soal-frontend/src/services/api.services.js
+++ bank-soal-frontend/src/services/api.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AuthService from "./auth.service";
 
-const API_BASE_URL = "http://sibasotest-production.up.railway.app/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/`;
 
 // Create axios instance
 const api = axios.create({
