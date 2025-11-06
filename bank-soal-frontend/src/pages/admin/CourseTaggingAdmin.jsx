2850341c8c7ef5c1c454fa77450b14e1d0023cd0
ClarenceFer <ferdinandclarence5@gmail.com>
2025-11-06 16:43:14 +0700

    Memperbaiki import Logi IF

--- bank-soal-frontend/src/pages/admin/CourseTaggingAdmin.jsx
+++ bank-soal-frontend/src/pages/admin/CourseTaggingAdmin.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import LogoIF from '../../assets/LogoIF.jpg';
 import axios from 'axios';
 import AuthService from '../../services/auth.service';
+import LogoIF from "../assets/LogoIF.jpg";
 
 const API_URL = "https://sibasotest-production.up.railway.app/api";
 
